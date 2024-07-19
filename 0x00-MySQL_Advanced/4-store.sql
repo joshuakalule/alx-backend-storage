36b4c4201f618c6d96da84d5302eb250aa77bb18
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-19 08:30:40 +0000

    add files for 4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,18 @@
+-- Task 4: Buy buy buy
+-- Creates a trigger that decreases the quantity of an item
+-- after buying it
+DELIMITER $$
+USE holberton
+CREATE TRIGGER update_items
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    DECLARE diff INT;
+    DECLARE old_quantity INT;
+    SELECT quantity FROM items WHERE name = NEW.item_name INTO old_quantity;
+    SET diff = old_quantity - NEW.number;
+    UPDATE items
+    SET quantity = IF(diff > 0, diff, 0)
+    WHERE name = NEW.item_name;
+END$$
+DELIMITER ;
