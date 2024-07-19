fad79e745e4350015e840f00ceb6f2c4edcf6f35
Kalule Joshua <kaljoshkalule@gmail.com>
2024-07-19 17:39:58 +0000

    add files for task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Task 5: Email validation to sent
+DELIMITER $$
+CREATE TRIGGER update_state
+BEFORE UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email
+    THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+DELIMITER ;
